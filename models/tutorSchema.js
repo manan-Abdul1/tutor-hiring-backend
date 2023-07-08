b4e950d5266d44eeeb3d8d872b93482ad0d50396
Abdul Manan <manansheikh117@gmail.com>
2023-07-08 23:07:12 +0500

    Working on Tutor Schema

--- models/tutorSchema.js
+++ models/tutorSchema.js
@@ -1,5 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const tutorSchema = mongoose.model({
-    
-})
+const tutorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Tutor = mongoose.model("Tutor", tutorSchema);
+
+module.exports = Tutor;
