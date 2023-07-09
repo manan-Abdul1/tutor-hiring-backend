0bfa0268399c753a8773ce11867db53c7b68444a
Abdul Manan <manansheikh117@gmail.com>
2023-07-09 22:49:16 +0500

    Tutor Schema Done

--- models/tutorSchema.js
+++ models/tutorSchema.js
@@ -1,7 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const tutorSchema = new mongoose.Schema({
   name: {
+    type: String,
+    required: true,
+  },
+  phone: {
     type: String,
     required: true,
   },
@@ -10,12 +14,86 @@ const tutorSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  cnic: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   password: {
     type: String,
     required: true,
   },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  timing: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: String,
+    required: true,
+  },
+  currentTeachInstitute: {
+    type: String,
+    required: true,
+  },
+  alumni: {
+    type: String,
+    required: true,
+  },
+  education: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+  classes: {
+    type: [String],
+    required: true,
+  },
+  subjects: {
+    type: [String],
+    required: true,
+  },
+  allSubjectFee: {
+    type: Number,
+    required: true,
+  },
+  perSubjectFee: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    enum: ['physical', 'online', 'both'],
+    required: true,
+  },
 });
 
-const Tutor = mongoose.model("Tutor", tutorSchema);
+const Tutor = mongoose.model('Tutor', tutorSchema);
 
 module.exports = Tutor;
