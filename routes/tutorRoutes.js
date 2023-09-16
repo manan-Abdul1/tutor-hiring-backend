a74dbb6e9804ca9e11dc129b7e547bc375ba3c27
Abdul <manansheikh117@gmail.com>
2023-09-16 12:37:55 +0500

    Handle getAllTeachers routes

--- routes/tutorRoutes.js
+++ routes/tutorRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerTutor,tutorsLogin } = require('../controllers/tutorController');
+const { registerTutor,tutorsLogin, getAllTeachers } = require('../controllers/tutorController');
 
 // Define the routes for Teachers
 router.post('/register', registerTutor);
 router.post('/tutors-login', tutorsLogin);
+router.get('/get-all-teachers', getAllTeachers);
 
 
 module.exports = router;
