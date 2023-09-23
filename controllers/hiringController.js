713491eaa0de14432d7cd1d254bec70d32c61468
Abdul <manansheikh117@gmail.com>
2023-09-24 01:01:31 +0500

    Add the create Hiring request

---
+++ controllers/hiringController.js
@@ -0,0 +1,33 @@
+const HiringRequest = require('../models/appointmentSchema'); 
+
+// Handle the POST request to create a new hiring request
+const createHiringRequest = async (req, res) => {
+  try {
+    const { studentId, teacherId, location, timing, topic, payment } = req.body;
+
+    // Create a new hiring request
+    const newRequest = new HiringRequest({
+      studentId,
+      teacherId,
+      location,
+      timing,
+      topic,
+      payment,
+      status: 'pending',
+      createdAt: new Date().toISOString(), 
+    });
+
+    // Save the request to the database
+    await newRequest.save();
+
+    // Respond with a success message or the newly created request
+    res.status(201).json({ message: 'Hiring request created successfully', request: newRequest });
+  } catch (error) {
+    console.error('Error creating hiring request:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  createHiringRequest,
+};
