7d9320f3bba46ef1c54b2b0fe95838d1afafde35
Abdul <manansheikh117@gmail.com>
2023-09-24 01:05:56 +0500

    Add hiring routes

---
+++ routes/hiringRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {createHiringRequest} = require('../controllers/hiringRequestController');
+
+// Define the routes for students
+router.post('/', createHiringRequest);
+
+
+module.exports = router;
