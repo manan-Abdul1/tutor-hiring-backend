1af76b80d684d7bc38a5193c3acc26ce92767564
Abdul <manansheikh117@gmail.com>
2023-09-24 00:40:11 +0500

    Improve the name of appointment Schema

--- models/appointmentSchema.js
+++ models/appointmentSchema.js
@@ -1,32 +1,32 @@
 const mongoose = require('mongoose');
 
-const formDataSchema = new mongoose.Schema({
-  studentId: {
-    type:String,
-    required:true,
-  },
-  teacherId: {
-    type:String,
-    required:true,
-  },
-  status: {
-        type:String,
-        required:true,
-  },
-  message: {
-    type:String,
-  },
-  createdAt: {
-    type:String,
-    // ISODate("timestamp"),
-  },
-  updatedAt: {
-    type:String,
-    // ISODate("timestamp"),
-  }
+const appointmentSchema = new mongoose.Schema({
+    studentId: {
+        type: String,
+        required: true,
+    },
+    teacherId: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+    },
+    createdAt: {
+        type: String,
+        // ISODate("timestamp"),
+    },
+    updatedAt: {
+        type: String,
+        // ISODate("timestamp"),
+    }
 });
 
 // Create a model based on the schema
-const FormData = mongoose.model('FormData', formDataSchema);
+const appointmentData = mongoose.model('Appointments', appointmentSchema);
 
-module.exports = FormData;
+module.exports = appointmentData;
