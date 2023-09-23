5723b5e0c07a6f44cc328de825cf99b87d0333f9
Abdul <manansheikh117@gmail.com>
2023-09-24 01:08:31 +0500

    Add hiring request routes to server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const studentRoutes = require('./routes/studentRoutes');
 const tutorRoutes = require('./routes/tutorRoutes');
+const hiringRequestRoutes = require('./routes/hiringRoutes');
 const dbconfig = require('./connection/db');
 
 const app = express();
@@ -13,5 +14,6 @@ app.use(cors());
 
 app.use('/api/users', studentRoutes);
 app.use('/api/tutors', tutorRoutes);
+app.use('/api/hiringRequest', hiringRequestRoutes);
 
 app.listen(port, ()=> console.log("server started",port))
