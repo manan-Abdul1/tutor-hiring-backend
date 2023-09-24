f03a6e48107642960ca40a0ad1bee12c7ff7fc63
Abdul <manansheikh117@gmail.com>
2023-09-25 00:37:32 +0500

    Add get Teacher Requests routes and controller

--- controllers/hiringRequestController.js
+++ controllers/hiringRequestController.js
@@ -27,33 +27,33 @@ const createHiringRequest = async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 };
-const findTeacherRequestsById = async (req, res) => {
+const getTeacherRequestsById = async (req, res) => {
   try {
-    const { tutorId } = req.body;
+    const teacherId = req.query.id;
+    console.log(teacherId, 'request');
 
-    // Create a new hiring request
-    const newRequest = new HiringRequest({
-      studentId,
-      teacherId,
-      location,
-      timing: new Date(timing),
-      topic,
-      payment,
-      status: 'pending',
-      createdAt: new Date().toISOString(), 
+    const requests = await HiringRequest.find({ teacherId });
+    console.log(requests, 'request');
+
+    if (!requests || requests.length === 0) {
+      return res.status(404).json({ message: 'No requests found', ok: false });
+    }
+
+    res.status(200).json({
+      requests,
+      message: 'Requests retrieved successfully',
+      ok: true,
     });
-
-    // Save the request to the database
-    await newRequest.save();
-
-    // Respond with a success message or the newly created request
-    res.status(201).json({ message: 'Hiring request created successfully', request: newRequest });
   } catch (error) {
-    console.error('Error creating hiring request:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error('Error retrieving teacher requests by teacherId:', error);
+    res.status(500).json({ message: 'Error retrieving requests', ok: false });
   }
 };
 
+
+
+
 module.exports = {
   createHiringRequest,
+  getTeacherRequestsById
 };
--- routes/hiringRoutes.js
+++ routes/hiringRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createHiringRequest } = require('../controllers/hiringRequestController');
+const { createHiringRequest, getTeacherRequestsById } = require('../controllers/hiringRequestController');
 
 // Define the routes for students
 router.post('/', createHiringRequest);
+router.get('/getTeacherRequestsById', getTeacherRequestsById);
 
 
 module.exports = router;
