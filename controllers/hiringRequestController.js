c693432ff6709c06a36c975b0beb7c2e0d9fef27
Abdul <manansheikh117@gmail.com>
2023-09-25 23:18:51 +0500

    Working on update request status

--- controllers/hiringRequestController.js
+++ controllers/hiringRequestController.js
@@ -48,7 +48,27 @@ const getTeacherRequestsById = async (req, res) => {
     res.status(500).json({ message: 'Error retrieving requests', ok: false });
   }
 };
+const updateRequestStatus = async (req, res) => {
+  try {
+    const teacherId = req.query.id;
 
+    // const requests = await HiringRequest.find({ teacherId });
+    const requests = await HiringRequest.find({ teacherId }).populate('studentId');
+
+    if (!requests || requests.length === 0) {
+      return res.status(404).json({ message: 'No requests found', ok: false });
+    }
+
+    res.status(200).json({
+      requests,
+      message: 'Requests retrieved successfully',
+      ok: true,
+    });
+  } catch (error) {
+    console.error('Error retrieving teacher requests by teacherId:', error);
+    res.status(500).json({ message: 'Error retrieving requests', ok: false });
+  }
+};
 
 
 
