047afa0b8d7e9d4e9f80509ae5871ee5a29e27dd
Abdul <manansheikh117@gmail.com>
2023-09-30 19:17:50 +0500

    handle all routes and controllers

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { createNotification, getNotificationsByUserId, markNotificationAsRead, deleteNotification } = require('../controllers/notificationController');
 const router = express.Router();
 
 // Define the routes for notifications
 router.post('/', createNotification);
-router.get('/', getNotifcaitonsByUserId);
-router.put('/mark-read', markNotificaitonAsRead);
-router.delete('/deleteNotification', deleteNoticaiton);
+router.get('/', getNotificationsByUserId);
+router.put('/mark-read', markNotificationAsRead);
+router.delete('/deleteNotification', deleteNotification);
 
 module.exports = router;
