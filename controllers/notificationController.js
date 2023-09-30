44ff5b520fb85a03a5abcbb18f5518c34277b57e
Abdul <manansheikh117@gmail.com>
2023-09-30 19:16:11 +0500

    Handle Notification controllers

---
+++ controllers/notificationController.js
@@ -0,0 +1,74 @@
+const Notification = require('../models/notificationSchema');
+// Create a new notification
+const createNotification = async (req, res) => {
+    try {
+      const { userId, message } = req.body;
+      const newNotification = new Notification({
+        userId,
+        message,
+        isRead: false,
+      });
+      await newNotification.save();
+      res.status(201).json(newNotification);
+    } catch (error) {
+      console.error('Error creating notification:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+  
+  // Get notifications by user ID
+  const getNotificationsByUserId = async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const notifications = await Notification.find({ userId });
+      res.status(200).json(notifications);
+    } catch (error) {
+      console.error('Error fetching notifications:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+  
+  // Mark notification as read
+  const markNotificationAsRead = async (req, res) => {
+    try {
+      const notificationId = req.params.notificationId;
+      const notification = await Notification.findById(notificationId);
+      
+      if (!notification) {
+        return res.status(404).json({ message: 'Notification not found', ok: false });
+      }
+  
+      notification.isRead = true;
+      await notification.save();
+  
+      res.status(200).json({ message: 'Notification marked as read', ok: true });
+    } catch (error) {
+      console.error('Error marking notification as read:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+  
+  // Delete a notification
+  const deleteNotification = async (req, res) => {
+    try {
+      const notificationId = req.params.notificationId;
+      const notification = await Notification.findById(notificationId);
+  
+      if (!notification) {
+        return res.status(404).json({ message: 'Notification not found', ok: false });
+      }
+  
+      await notification.remove();
+  
+      res.status(200).json({ message: 'Notification deleted successfully', ok: true });
+    } catch (error) {
+      console.error('Error deleting notification:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+module.exports = {
+    createNotification,
+    getNotificationsByUserId,
+    markNotificationAsRead,
+    deleteNotification,
+};
