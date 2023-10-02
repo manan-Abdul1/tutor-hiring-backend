6215f55835764b5d55350d34639e507291438961
Abdul <manansheikh117@gmail.com>
2023-10-02 23:47:20 +0500

    Delete Feedback by Id

--- controllers/FeedbackController.js
+++ controllers/FeedbackController.js
@@ -49,6 +49,21 @@ const getFeedbackByUser = async (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     }
   };
+
+// Delete a feedback entry by ID
+const deleteFeedback = async (req, res) => {
+    try {
+      const feedbackId = req.params.feedbackId;
+  
+      await Feedback.findByIdAndDelete(feedbackId);
+  
+      res.status(200).json({ message: 'Feedback deleted successfully', ok: true });
+    } catch (error) {
+      console.error('Error deleting feedback:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+  
 module.exports = {
   createFeedback,
   getFeedbackForTeacher
